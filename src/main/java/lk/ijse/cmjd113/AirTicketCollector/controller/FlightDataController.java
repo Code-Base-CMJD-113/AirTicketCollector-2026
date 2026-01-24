d986a59abaee766dff190cbca12c4012ed962f0d
ThanuraSilva <thanurasilva1989@gmail.com>
2026-01-25 00:28:17 +0530

    Add DTOs and BookingService with BookingDataController

---
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/FlightDataController.java
@@ -0,0 +1,44 @@
+package lk.ijse.cmjd113.AirTicketCollector.controller;
+
+
+import com.thanu.home.airtickter.dto.FlightDTO;
+import com.thanu.home.airtickter.service.FlightService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/flight")
+@RequiredArgsConstructor
+public class FlightDataController {
+
+  private final FlightService flightService;
+
+  @GetMapping("/{flightId}")
+  public ResponseEntity<FlightDTO> getSelectedFlight(@PathVariable String flightId) {
+    return new ResponseEntity<>(flightService.getFlight(flightId), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<Void> saveFlight(@RequestBody FlightDTO flight) {
+    flightService.saveFlight(flight);
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+  @GetMapping
+  public  ResponseEntity<List<FlightDTO>> getAllFlights(){
+    return new ResponseEntity<>(flightService.getAllFlights(), HttpStatus.OK);
+  }
+  @DeleteMapping("/{flightId}")
+  public ResponseEntity<Void> deleteFlight(@PathVariable String flightId){
+    flightService.deleteFlight(flightId);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+  @PatchMapping("/{flightId}")
+  public ResponseEntity<Void> updatePassenger(@PathVariable String flightId,@RequestBody FlightDTO flight){
+    flightService.updateFlight(flightId, flight);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+}
---
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/UserDataController.java
@@ -0,0 +1,49 @@
+package lk.ijse.cmjd113.AirTicketCollector.controller;
+
+import com.thanu.home.airtickter.dto.UserDTO;
+import com.thanu.home.airtickter.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserDataController {
+
+  private final UserService userService;
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<UserDTO> getSelectedUser(@PathVariable String userId) {
+    return new ResponseEntity<>(userService.getUser(userId), HttpStatus.OK);
+  }
+  @PostMapping
+  public ResponseEntity<String> saveUser(@RequestBody UserDTO userDTO) {
+//    try {
+//      userService.saveUser(userDTO);
+//      return new ResponseEntity<>(HttpStatus.CREATED);
+//    }catch (DataSaveException e){
+//      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//              .body(e.getMessage());
+//    }
+        userService.saveUser(userDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+  @GetMapping
+  public  ResponseEntity<List<UserDTO>> getAllUsers(){
+    return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+  }
+  @DeleteMapping("/{userId}")
+  public ResponseEntity<Void> deleteUser(@PathVariable String userId){
+    userService.deleteUser(userId);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+  @PatchMapping("/{userId}")
+  public ResponseEntity<Void> updateUser(@PathVariable String userId,@RequestBody UserDTO userDto){
+    userService.updateUser(userId, userDto);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+}
