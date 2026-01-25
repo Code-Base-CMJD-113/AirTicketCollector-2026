93b4eaa0dba14937a9c79300503187e275bfe31f
ThanuraSilva <thanurasilva1989@gmail.com>
2026-01-25 14:22:30 +0530

    Add model mapper

--- src/main/java/lk/ijse/cmjd113/AirTicketCollector/AirTicketCollectorApplication.java
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/AirTicketCollectorApplication.java
@@ -1,13 +1,19 @@
 package lk.ijse.cmjd113.AirTicketCollector;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AirTicketCollectorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AirTicketCollectorApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
