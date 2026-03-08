f3e8198e1a0d04ddc659a47f3d2de0ec6d13bd0c
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-08 13:04:37 +0530

    Add UserDetailServiceIMPL

--- src/main/java/lk/ijse/cmjd113/AirTicketCollector/dao/UserDao.java
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/dao/UserDao.java
@@ -2,6 +2,11 @@ package lk.ijse.cmjd113.AirTicketCollector.dao;
 
 import lk.ijse.cmjd113.AirTicketCollector.entities.secure.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserDao extends JpaRepository<UserEntity,String> {
+    Optional<UserEntity> findByEmail(String email);
 }
