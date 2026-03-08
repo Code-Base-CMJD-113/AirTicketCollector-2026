070775fea1d0969724a0d9b198936b7da9976784
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-08 14:55:26 +0530

    Complete AuthControllers

--- src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/secure/AuthController.java
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/secure/AuthController.java
@@ -3,6 +3,7 @@ package lk.ijse.cmjd113.AirTicketCollector.controller.secure;
 import lk.ijse.cmjd113.AirTicketCollector.dto.secure.JWTResponseDTO;
 import lk.ijse.cmjd113.AirTicketCollector.dto.secure.LoginDTO;
 import lk.ijse.cmjd113.AirTicketCollector.dto.secure.UserDTO;
+import lk.ijse.cmjd113.AirTicketCollector.service.secure.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    private  final AuthService authService;
     @PostMapping("/signup")
     public ResponseEntity<JWTResponseDTO> signUp(@RequestBody UserDTO user) {
-        return null;
+        return ResponseEntity.ok(authService.signUp(user));
     }
     @PostMapping("/login")
     public ResponseEntity<JWTResponseDTO> login(@RequestBody LoginDTO login) {
-        return null;
+        return ResponseEntity.ok(authService.login(login));
     }
 }
