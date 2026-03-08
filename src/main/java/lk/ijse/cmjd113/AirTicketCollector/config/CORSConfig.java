b22ef0fcdddeed027044a6b3f55b7aa72a0ac469
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-08 13:52:42 +0530

    Add WebSecurityConfig with CORSConfig modification

--- src/main/java/lk/ijse/cmjd113/AirTicketCollector/config/CORSConfig.java
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/config/CORSConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -11,8 +12,23 @@ import java.util.List;
 
 @Configuration
 public class CORSConfig {
+//    @Bean
+//    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+//        var corsConfig = new CorsConfiguration();
+//        corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
+//        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+//        corsConfig.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+//        corsConfig.setAllowCredentials(true);
+//        corsConfig.setExposedHeaders(List.of("Authorization", "Content-Type"));
+//        var corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+//        corsConfigurationSource.registerCorsConfiguration("/**", corsConfig);
+//
+//        var corsFilterBean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource));
+//        corsFilterBean.setOrder(0);
+//        return corsFilterBean;
+  //  }
     @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+    public CorsConfigurationSource corsConfigurationSource() {
         var corsConfig = new CorsConfiguration();
         corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
         corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
@@ -22,8 +38,10 @@ public class CORSConfig {
         var corsConfigurationSource = new UrlBasedCorsConfigurationSource();
         corsConfigurationSource.registerCorsConfiguration("/**", corsConfig);
 
-        var corsFilterBean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource));
-        corsFilterBean.setOrder(0);
-        return corsFilterBean;
+        var configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**", corsConfig);
+        return configurationSource;
     }
+
+
 }
---
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/securityConfig/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package lk.ijse.cmjd113.AirTicketCollector.securityConfig;
+
+import lk.ijse.cmjd113.AirTicketCollector.config.CORSConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final AuthEntryPoint authEntryPoint;
+    private final AuthFilter authFilter;
+    private final CORSConfig corsConfig;
+
+    //PW Encoder
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    //Auth Provider
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration){
+        return configuration.getAuthenticationManager();
+    }
+    //Filter Chain
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors-> cors.configurationSource(corsConfig.corsConfigurationSource()))
+                .exceptionHandling(exception ->
+                        exception.authenticationEntryPoint(authEntryPoint))
+
+                .sessionManagement(session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests( auth ->
+                        auth.requestMatchers("/api/v1/auth/**").permitAll()
+                                .anyRequest().authenticated())
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
