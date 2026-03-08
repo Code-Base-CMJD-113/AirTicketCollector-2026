ef870fe5a27a9f7dda728d8ba2e3b60459db1736
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-08 12:24:30 +0530

    Test Basic Auth with TestFilter

---
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/config/TestFilter.java
@@ -0,0 +1,18 @@
+package lk.ijse.cmjd113.AirTicketCollector.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+public class TestFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println(request.getHeader("Authorization"));
+       filterChain.doFilter(request,response);
+    }
+}
